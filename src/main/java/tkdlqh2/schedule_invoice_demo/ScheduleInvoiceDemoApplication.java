333f9115903d0d8b6cdc60d2b3d454f3da348522
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2025-12-13 19:27:12 +0900

    Feat: first commit

---
+++ src/main/java/tkdlqh2/schedule_invoice_demo/ScheduleInvoiceDemoApplication.java
@@ -0,0 +1,13 @@
+package tkdlqh2.schedule_invoice_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleInvoiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleInvoiceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tkdlqh2/schedule_invoice_demo/ScheduleInvoiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tkdlqh2.schedule_invoice_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleInvoiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
