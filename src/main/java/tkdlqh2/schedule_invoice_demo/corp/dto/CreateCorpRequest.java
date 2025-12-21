61fd4209ae5853fb9a0e696b7f099e08b09960c8
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2025-12-21 13:45:23 +0900

    Fix: Validation 추가

--- src/main/java/tkdlqh2/schedule_invoice_demo/corp/dto/CreateCorpRequest.java
+++ src/main/java/tkdlqh2/schedule_invoice_demo/corp/dto/CreateCorpRequest.java
@@ -1,8 +1,15 @@
 package tkdlqh2.schedule_invoice_demo.corp.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record CreateCorpRequest(
+        @NotBlank(message = "기관명은 필수입니다")
         String name,
+        @NotBlank(message = "사업자번호는 필수입니다")
+        @Pattern(regexp = "\\d{3}-\\d{2}-\\d{5}", message = "사업자번호 형식이 올바르지 않습니다")
         String businessNumber,
+        @NotBlank(message = "연락처는 필수입니다")
         String contactPhone
 ) {
 }
